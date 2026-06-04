4f498e6fbcab7653a2dba9db04698fcbb66de616
Jordan-Home-PC <breannajhollins@gmail.com>
2026-06-03 23:33:14 -0400

    Created cards to be flipped

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,38 @@
+import { Card } from "./components/Card";
 import { GameHeader } from "./components/gameHeader"
+
+const cardValues = [
+  "🐳",
+  "🦚",
+  "🐢",
+  "🐈",
+  "🐩",
+  "🦔",
+  "🐠",
+  "🐇",
+  "🐳",
+  "🦚",
+  "🐢",
+  "🐈",
+  "🐩",
+  "🦔",
+  "🐠",
+  "🐇",
+];
+
 
 function App() {
   return (
     <div className="app">
-      <GameHeader/>
+      <GameHeader score={3} moves={10}/>
+
+      <div className="cards-grid">
+        {cardValues.map(card => (
+          <Card card={card} />
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
 export default App
--- src/components/GameHeader.jsx
+++ src/components/GameHeader.jsx
@@ -1,16 +1,14 @@
-
-
-export const GameHeader = () => {
+export const GameHeader = ({score, moves}) => {
     return <div className="game-header">
         <h1>🕹️ Memory Game</h1>
         <div className="stats">
             <div className="stat-item">
                 <span className="stat-label">Score:</span>{" "}
-                <span className="stat-value">0</span>
+                <span className="stat-value">{score}</span>
             </div>
             <div className="stat-item">
                 <span className="stat-label">Moves:</span>{" "} 
-                <span className="stat-value">0</span>
+                <span className="stat-value">{moves}</span>
             </div>
         </div>
     </div>
