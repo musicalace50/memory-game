8f7e72ddd80a161545b4d39aa30166ac9e76f527
Jordan-Home-PC <breannajhollins@gmail.com>
2026-06-08 19:09:46 -0400

    Forgot to save shuffleArray :')

--- src/App.jsx
+++ src/App.jsx
@@ -30,6 +30,15 @@ function App() {
   const [moves, setMoves] = useState(0);
   const [isLocked, setLocked] = useState(false);
 
+  const shuffleArray = (array) => {
+    const shuffled = [...array];
+    for (let i = shuffled.length -1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+    }// Ends shuffle for loop
+    return shuffled;
+  };
+
 const initGame = () => {
   //Shuffle cards
 
